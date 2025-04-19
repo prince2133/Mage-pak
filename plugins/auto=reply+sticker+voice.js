cec457fbfa2d591adf1dc06d8a48c653c2fd0858
prince2133 <ashuunew4@gmail.com>
2025-04-19 10:20:30 +0530

    Update auto=reply+sticker+voice.js

--- plugins/auto=reply+sticker+voice.js
+++ plugins/auto=reply+sticker+voice.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const config = require('../config')
 const path = require('path');
 const {readEnv} = require('../lib/database')
+const EnvVar = require('./lib/mongodbenv');
 const {cmd , commands} = require('../command')
 
 //auto_voice
